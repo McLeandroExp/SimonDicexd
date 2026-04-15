2a53bbdc68642f744864a72dd0d943c6df7769aa
Leandro_Arguello <leandro@innova3ec.com>
2026-04-15 18:53:57 -0500

    tres cambios

--- js/simonDice.js
+++ js/simonDice.js
@@ -51,6 +51,7 @@ export default function simonDice(botones, botonesMostrados, focos) {
             }, 700);
         });
     }
+    
     // muestra el patron en el panel de muestra
     const mostrarPatron = (arregloMostr) => {
         desactivarBotones();
